9acfa10d1faaf434ac816886b828810815169904
Max Betich <betichm@gmail.com>
2023-04-13 11:21:56 -0700

    add characterGenerator test

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -9,5 +9,14 @@ describe ('Character', () => {
     expect(newCharacter.strength).toEqual(0);
     expect(newCharacter.intelligence).toEqual(0);
     expect(newCharacter.agility).toEqual(0);
+  });
+
+  test('should create a Character object with inputted properties for name, health, strength, intelligence, and agility', () => {
+    const newCharacter = new Character("character one");
+    newCharacter.characterGenerator(1,2,3,4);
+    expect(newCharacter.health).toEqual(1);
+    expect(newCharacter.strength).toEqual(2);
+    expect(newCharacter.intelligence).toEqual(3);
+    expect(newCharacter.agility).toEqual(4);
   })
 })
